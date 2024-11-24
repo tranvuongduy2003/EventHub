d9e1bac69e784c19775deddf0244e41df8d15994
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2024-11-24 12:57:30 +0700

    Migrate to Gitea

--- source/EventHub.Presentation/Program.cs
+++ source/EventHub.Presentation/Program.cs
@@ -11,30 +11,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 Log.Information("Starting EvenHub API up");
 
-try
-{
-    builder.Host.UseSerilog(LoggingConfiguration.Configure);
-    builder.Host.AddAppConfigurations();
+builder.Host.UseSerilog(LoggingConfiguration.Configure);
+builder.Host.AddAppConfigurations();
 
-    builder.Services.ConfigurePersistenceServices(builder.Configuration);
-    builder.Services.ConfigureInfrastructureServices(builder.Configuration, AppCors);
-    builder.Services.ConfigureSignalRServices(builder.Configuration);
+builder.Services.ConfigurePersistenceServices(builder.Configuration);
+builder.Services.ConfigureInfrastructureServices(builder.Configuration, AppCors);
+builder.Services.ConfigureSignalRServices(builder.Configuration);
 
-    var app = builder.Build();
+var app = builder.Build();
 
-    app.UseInfrastructure(AppCors);
+app.UseInfrastructure(AppCors);
 
-    app.Run();
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, $"Unhandled exception: {ex.Message}");
-
-    var type = ex.GetType().Name;
-    if (type.Equals("StopTheHostException", StringComparison.Ordinal)) throw;
-}
-finally
-{
-    Log.Information("Shut down EventHub API complete");
-    Log.CloseAndFlush();
-}
+app.Run();
