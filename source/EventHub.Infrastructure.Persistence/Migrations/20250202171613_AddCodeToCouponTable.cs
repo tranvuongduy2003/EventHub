0ddde30e2a0261ebe959f1eae2c83f18ac7b90b8
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2025-02-03 00:16:33 +0700

    add migrations

---
+++ source/EventHub.Infrastructure.Persistence/Migrations/20250202171613_AddCodeToCouponTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventHub.Infrastructure.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class AddCodeToCouponTable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "Code",
+            table: "Coupons",
+            type: "nvarchar(max)",
+            nullable: false,
+            defaultValue: "");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "Code",
+            table: "Coupons");
+    }
+}
