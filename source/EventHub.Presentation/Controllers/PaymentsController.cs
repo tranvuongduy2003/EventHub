fd8d3fab7b78133e691a7b3931d36d49340bf597
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2025-02-03 19:16:45 +0700

    fix

--- source/EventHub.Presentation/Controllers/PaymentsController.cs
+++ source/EventHub.Presentation/Controllers/PaymentsController.cs
@@ -1,8 +1,5 @@
 ﻿using EventHub.Application.Commands.Payment.Checkout;
-using EventHub.Application.SeedWork.Attributes;
 using EventHub.Application.SeedWork.DTOs.Payment;
-using EventHub.Domain.Shared.Enums.Command;
-using EventHub.Domain.Shared.Enums.Function;
 using EventHub.Domain.Shared.HttpResponses;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -13,8 +10,8 @@ namespace EventHub.Presentation.Controllers;
 [ApiController]
 public class PaymentsController : ControllerBase
 {
-    private readonly IMediator _mediator;
     private readonly ILogger<PaymentsController> _logger;
+    private readonly IMediator _mediator;
 
     public PaymentsController(ILogger<PaymentsController> logger, IMediator mediator)
     {
@@ -23,33 +20,11 @@ public class PaymentsController : ControllerBase
     }
 
     [HttpPost("checkout")]
-    [ClaimRequirement(EFunctionCode.GENERAL_PAYMENT, ECommandCode.CREATE)]
     public async Task<IActionResult> PostCheckout([FromBody] CheckoutDto request)
     {
         _logger.LogInformation("START: PostCheckout");
 
-        CheckoutResponseDto response = await _mediator.Send(new CheckoutCommand
-        {
-            CustomerName = request.CustomerName,
-            CustomerEmail = request.CustomerEmail,
-            CustomerPhone = request.CustomerPhone,
-            EventId = request.EventId,
-            UserId = request.UserId,
-            TotalPrice = request.TotalPrice,
-            CheckoutItems = request.CheckoutItems
-                .Select(x => new CheckoutItemCommand
-                {
-                    Name = x.Name,
-                    EventId = request.EventId,
-                    Price = x.Price,
-                    Quantity = x.Quantity,
-                    TicketTypeId = x.TicketTypeId,
-                })
-                .ToList(),
-            CouponId = request.CouponId,
-            SuccessUrl = request.SuccessUrl,
-            CancelUrl = request.CancelUrl,
-        });
+        CheckoutResponseDto response = await _mediator.Send(new CheckoutCommand(request));
 
         _logger.LogInformation("END: PostCheckout");
 
